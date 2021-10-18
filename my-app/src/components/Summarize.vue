e76f27bed1f482d7c39a35bc69de4c5dca36b662
Imane Chatri <70664538+ImaneChatri@users.noreply.github.com>
2021-10-18 14:01:28 -0500

    Fixed layout, still need overlay for notif

--- my-app/src/components/Summarize.vue
+++ my-app/src/components/Summarize.vue
@@ -29,23 +29,19 @@
       </div>
       <div class="tile is-parent is-vertical">
         <article class="tile is-child notification is-primary">
-          <div class="field is-grouped">
-            <p class="control">
+          <div class="buttons are-normal is-centered">
               <button class="button is-medium" @click="summarize()">
                 Summarize
               </button>
-            </p>
-            <p class="control">
               <button class="button is-medium" @click="clear()">
                 Clear
               </button>
-            </p>
           </div>
+          <div class="notification is-info is-light is-overlay" v-if="Hover">
+            Choose <strong>extractive</strong> if you want to generate a summary with preserved sentences from the original text, and <strong>abstractive</strong> if you want computer generated sentences in your summary.
+        </div>
         </article>
         <article class="tile is-child notification is-warning">
-          <div class="notification is-info is-light" v-if="Hover">
-            Choose <strong>extractive</strong> if you want to generate a summary with preserved sentences from the original text, and <strong>abstractive</strong> if you want computer generated sentences in your summary.
-          </div>
           <article class="message">
             <div class="message-body">
               {{ Output }}
@@ -68,29 +64,16 @@ export default {
       Input:
         "Macalester College was founded by Edward Duffield Neill in 1874 with help from the Presbyterian Church in Minnesota. Neill had served as a chaplain in the Civil War and traveled to Minnesota Territory in 1849. He became connected politically and socially. He went on to found two local churches, was appointed the first Chancellor of the University of Minnesota and became the state's first superintendent of public education. In leaving the University of Minnesota Board of Regents he desired to build a religious college affiliated with the Presbyterian Church that would also be open to other Christian church members.[6] The college's original name was Baldwin College; it was affiliated with the Baldwin School, a Presbyterian secondary school.[7] After a large donation from Charles Macalester, a prominent businessman and philanthropist from Philadelphia, the institution was renamed Macalester College. Macalester donated a hotel, the Winslow House, as the first permanent classroom building. With additional funding from the Presbyterian Church and its trustees, Macalester College opened for courses in 1885 with five teachers, six freshmen, and 52 preparatory students.[6] James Wallace joined the faculty in 1887 and later became president. He helped stabilize the college's finances and advance the institution.[6] During his tenure, Macalester created a focus on a liberal arts curriculum.",
       Output: "Your summarized text will appear here",
+      Method: 'Extractive',
       Hover: false,
     };
   },
   methods: {
     summarize() {
-      // axios
-      //   .post(`http://127.0.0.1:5000/summarize`, {"text":this.Input})
-      //   .then((response) => {
-      //     this.Output = response.data;
-      //   })
-      //   .catch((e) => {
-      //     this.errors.push(e);
-      //   });
       axios
         .post(
           `http://127.0.0.1:5000/summarize`,
-          { text: this.Input },
-          {
-            // headers: {
-            //   "Access-Control-Allow-Origin": "*",
-            //   "Content-Type": "application/json",
-            // },
-          }
+          { text: this.Input }
         )
         .then((response) => {
           this.Output = response.data.summarizedText;
@@ -118,8 +101,10 @@ export default {
 </script>
 
 <style scoped>
-.notification.is-info.is-light {
+/* .notification.is-info.is-light {
   position: absolute;
   z-index: 10;
-}
+  top: 50%;
+  left: 50%;
+} */
 </style>
