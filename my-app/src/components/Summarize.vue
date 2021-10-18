aa6e784df8bb917c6e6bab6b42c8a0594f6a61ff
Imane Chatri <70664538+ImaneChatri@users.noreply.github.com>
2021-10-18 12:04:20 -0500

    added hoverable message

--- my-app/src/components/Summarize.vue
+++ my-app/src/components/Summarize.vue
@@ -6,25 +6,23 @@
           <textarea
             class="textarea"
             placeholder="Input your text here"
-            rows="13"
+            rows="15"
             v-model="Input"
           ></textarea>
           <br />
           <div class="buttons are-normal is-centered">
-            <button class="button" @click="NumSentences = '1'">
-              1
+            <button class="button" @click="SummaryType = 'Extractive'">
+              Extractive
             </button>
-            <button class="button" @click="NumSentences = '3'">
-              3
+            <button class="button" @click="SummaryType = 'Abstractive'" disabled>
+              Abstractive
             </button>
-            <button class="button" @click="NumSentences = '5'">
-              5
-            </button>
-            <button class="button" @click="NumSentences = '10'">
-              10
-            </button>
-            <button class="button" @click="NumSentences = '20'">
-              20
+            <button
+              class="button is-rounded is-focused"
+              @mouseover="Hover = true"
+              @mouseleave="Hover = false"
+            >
+              ?
             </button>
           </div>
         </article>
@@ -38,13 +36,16 @@
               </button>
             </p>
             <p class="control">
-              <button class="button is-medium">
+              <button class="button is-medium" @click="clear()">
                 Clear
               </button>
             </p>
           </div>
         </article>
         <article class="tile is-child notification is-warning">
+          <div class="notification is-info is-light" v-if="Hover">
+            Choose <strong>extractive</strong> if you want to generate a summary with preserved sentences from the original text, and <strong>abstractive</strong> if you want computer generated sentences in your summary.
+          </div>
           <article class="message">
             <div class="message-body">
               {{ Output }}
@@ -67,6 +68,7 @@ export default {
       Input:
         "Macalester College was founded by Edward Duffield Neill in 1874 with help from the Presbyterian Church in Minnesota. Neill had served as a chaplain in the Civil War and traveled to Minnesota Territory in 1849. He became connected politically and socially. He went on to found two local churches, was appointed the first Chancellor of the University of Minnesota and became the state's first superintendent of public education. In leaving the University of Minnesota Board of Regents he desired to build a religious college affiliated with the Presbyterian Church that would also be open to other Christian church members.[6] The college's original name was Baldwin College; it was affiliated with the Baldwin School, a Presbyterian secondary school.[7] After a large donation from Charles Macalester, a prominent businessman and philanthropist from Philadelphia, the institution was renamed Macalester College. Macalester donated a hotel, the Winslow House, as the first permanent classroom building. With additional funding from the Presbyterian Church and its trustees, Macalester College opened for courses in 1885 with five teachers, six freshmen, and 52 preparatory students.[6] James Wallace joined the faculty in 1887 and later became president. He helped stabilize the college's finances and advance the institution.[6] During his tenure, Macalester created a focus on a liberal arts curriculum.",
       Output: "Your summarized text will appear here",
+      Hover: false,
     };
   },
   methods: {
@@ -79,18 +81,27 @@ export default {
       //   .catch((e) => {
       //     this.errors.push(e);
       //   });
-        axios
-      .post(`http://127.0.0.1:5000/summarize`, {"text":this.Input}, {headers:{'Access-Control-Allow-Origin':'*','Content-Type': 'application/json'}})
-      .then((response) => {
-        this.Output = response.data;
-      })
-      .catch((e) => {
-        this.errors.push(e);
-      });
+      axios
+        .post(
+          `http://127.0.0.1:5000/summarize`,
+          { text: this.Input },
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          this.Output = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
     },
-    Clear() {
+    clear() {
       this.Input = "";
-      this.Output = "";
+      this.Output = "Your summarized text will appear here";
     },
   },
   // created() {
@@ -106,4 +117,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.notification.is-info.is-light {
+  position: absolute;
+  z-index: 10;
+}
+</style>
