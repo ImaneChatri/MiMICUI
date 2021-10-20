9f7dea7412907acf7f65991eaecac77d24cda9e5
meloarteaga <73600799+meloarteaga@users.noreply.github.com>
2021-10-20 02:24:26 -0500

    updated teams

--- my-app/src/components/Algorithm.vue
+++ my-app/src/components/Algorithm.vue
@@ -14,35 +14,35 @@
   </thead>
   <tbody>
     <tr>
-      <th>Preprocessing</th>
-      <td>sent_tokenizer: splits text into lists of words, each list representing a sentence.</td>
+      <th>sent_tokenizer: </th>
+      <td>splits text into lists of words, each list representing a sentence.</td>
     </tr>
     <tr>
-       <th>Preprocessing</th>
-      <td>Preproccess: removes punctuation and stopwords such as ‘the,’ ‘and,’ ‘or,’ etc.</td>
+       <th>preproccess: </th>
+      <td>removes punctuation and stopwords such as ‘the,’ ‘and,’ ‘or,’ etc.</td>
     </tr>
     <tr>
-      <th>Preprocessing</th>
-      <td>tag_pos: ‘tags’ the grammatical category of words likely to carry important information, such as nouns or past participles
+      <th>tag_pos: </th>
+      <td>‘tags’ the grammatical category of words likely to carry important information, such as nouns or past participles
 </td>
     </tr>
     <tr>
-      <th>Preprocessing</th>
-      <td>stem: strips tagged words to their most basic lexical form, or stem. For example, the word ‘cities’ would be stemmed to ‘city,’ or ‘gone’ to ‘go’
+      <th>stem: </th>
+      <td>strips tagged words to their most basic lexical form, or stem. For example, the word ‘cities’ would be stemmed to ‘city,’ or ‘gone’ to ‘go’
 </td>
     </tr>
     <tr>
-      <th>Similarity Comparison</th>
-      <td>Build_similarity_matrix: turns processed text into machine-readable vectors, then creates a cosine similarity matrix, comparing each sentence to each other
+      <th> build_similarity_matrix: </th>
+      <td>turns processed text into machine-readable vectors, then creates a cosine similarity matrix, comparing each sentence to each other
 </td>
     </tr>
     <tr>
-      <th>Ranking</th>
-       <td>text_rank: ranks sentences based on the previously created similarity matrix, giving the most favorable rankings to the sentences most similar to one another</td>
+      <th>text_rank: </th>
+       <td>ranks sentences based on the previously created similarity matrix, giving the most favorable rankings to the sentences most similar to one another</td>
     </tr>
     <tr>
-      <th>Summary Assembly</th>
-      <td>summarize: chooses top X  ranked sentences (X input by user), orders them chronologically as in the input text, puts a line break between each, and returns the summarized paragraph</td>
+      <th>summarize: </th>
+      <td>chooses top X  ranked sentences (X input by user), orders them chronologically as in the input text, puts a line break between each, and returns the summarized paragraph</td>
     </tr>
   </tbody>
 </table>
--- my-app/src/components/Team.vue
+++ my-app/src/components/Team.vue
@@ -1,33 +1,96 @@
 <template>
-<div class="tile is-ancestor">
+<div class="tile is-ancestor my-1">
   <div class="tile is-4 is-vertical is-parent">
-    <div class="tile is-child box">
-      <p class="title">Charles Zhang '23</p>
-      <p> Mathematics and Computer Science Major</p>
-      <figure class="image is-128x128">
+    <div class="tile is-child panel is-success">
+      <p class="panel-heading">Charles Zhang '23</p>
+      <div class="panel-block">
+      <div class="card-image has-text-centered">
+        <div class="columns">
+      <figure class="image is-128x128 is-inline-block">
+    <div class="column is-11">
   <img src="https://github.com/zcczhang/zcczhang.github.io/blob/master/images/profile.png?raw=true">
+  </div>
 </figure>
+ <div class="column is-7 ">
+  <div class="myHero hero-right">
+        <div class="hr"></div>
+        <h1>Computer Science and Mathematics Major</h1>
+  </div>
+  </div>
+  </div>
+</div>
+</div>
     </div>
-    <div class="tile is-child box">
-      <p class="title">Imane Chatri</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+    <div class="tile is-child panel is-primary">
+      <p class="panel-heading">Imane Chatri '23</p>
+      <div class="panel-block">
+      <div class="card-image has-text-centered">
+        <div class="columns">
+      <figure class="image is-128x128 is-inline-block">
+          <div class="column is-13">
+  <img src="https://blogger.googleusercontent.com/img/a/AVvXsEhR-MdG4DkTLDy_5uITULOrK11kYBsqI_LxSvhoountnHhgbpFag3IGRSdpHCgWM3pDq3yDC0ItF2lY7iq6BA_iHH17TTMthaUMdj4ouwdKZDzyX-40LsI7COIg8L5oUfOOy847sx0HoQc8KCK64oHHSt1ZTliHFQnut2BHwtJQWUFeF9M0Sy3a2jU_=s320">
+</div>
+</figure>
+<div class="column is-7 ">
+  <div class="myHero hero-right">
+        <div class="hr"></div>
+        <h1>Computer Science and Linguistics Major</h1>
+</div>
+</div>
+</div>
+</div>
+</div>
     </div>
-    <div class="tile is-child box">
-      <p class="title">Matthew Wilkinson</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+    <div class="tile is-child panel is-info">
+      <p class="panel-heading">Matthew Wilkinson '22</p>
+      <div class="panel-block">
+      <div class="card-image has-text-centered">
+        <div class="columns">
+      <figure class="image is-128x128 is-inline-block">
+          <div class="column is-12">
+  <img src="https://blogger.googleusercontent.com/img/a/AVvXsEjy0sNhJ16_zp-zJNJl6sSuYAn40Dw4LTIbDU_Kepw-hICdf5GZwKAFwVqVjQSAj80v9g7yTHMe0lLXJrO9RfQGmSSIe-CD_sV8Gxbty_REnFnEnVXAj4kdQmPO2veIqeIFGVZ9WyZjG2WlkmeuBdyu0F4c9NG-6tpmCMhVqCXDU5vlIXMi67WVnz_d=s320">
+</div>
+</figure> 
+<div class="column is-7 ">
+  <div class="myHero hero-right">
+        <div class="hr"></div>
+        <h1>Linguistics Major</h1>
+</div>
+</div>
+</div>
+</div>
+</div>
     </div>
-    <div class="tile is-child box">
-      <p class="title">Melody Arteaga '23</p>
-      <p>Mathematics and Computer Science Major</p>
-      <!-- <figure class="image is-128x128">
-  <img src="./assets/MelodyPic.jpg">
-</figure> *not working-->
+    <div class="tile is-child panel is-link">
+      <p class="panel-heading">Melody Arteaga '23</p>
+      <div class="panel-block">
+      <div class="card-image has-text-centered">
+        <div class="columns">
+      <figure class="image is-128x128 is-inline-block">
+          <div class="column is-13">
+  <img src="https://blogger.googleusercontent.com/img/a/AVvXsEjnQ97ThdAIDF9DNP8z2wV-a69XG3q7dwgiA1lp7XujS7Hx6I81OSZH7iyrpH4u0m_kCg6YVxuhrhSDidgYwCOmgN5la2sVZz7cC0UC_ANyJ_EnazOhApwloWSrcOSMPQpm0oklGCvGv0uE4wl3xKpHe9AEJ9tpb2Vre6glDuu93BWKt9LrK3ENlieX=s320">
+   </div>
+</figure> 
+ <div class="column is-7">
+  <div class="myHero hero-right">
+        <div class="hr"></div>
+        <h1>Computer Science and Mathematics Major</h1>
+  </div>
+  </div>
+  </div>
+</div>
+</div>
     </div>
   </div>
   <div class="tile is-parent">
-    <div class="tile is-child box">
-      <p class="title">	COMP 225 Software Design and Development</p>
-      <p>This course is an introduction to the problem of building software with humans and for humans. Students work in teams to design and implement a semester-long user-facing software project of their own invention. There are no limitations on topic or technology; on the contrary, students are responsible for imagining possibilities, articulating goals, and researching and selecting suitable technologies. The format resembles a studio art class, with in-class discussion guided by sharing and critiquing classmates' ongoing work. Topics include communication, division of labor, user-centered design, human-computer interaction, product management, project management, iterative development, engineering tradeoffs, separation of concerns, code readability and maintainability, refactoring, testing, and version control. Teams give a public demonstration of their working projects at the end of the semester. Prerequisite(s): COMP 127 (COMP 128 recommended), or COMP 124 if previously taken, or permission of instructor.</p>
+    <div class="tile is-child panel is-warning">
+      <p class="panel-heading">	ABOUT THE PROJECT</p>
+      <div class="panel-block">
+        <p> MORE TO COME.... </p>
+      <!-- <p>This website was made for our computer science class, Software Design and Development. As liberal arts college students, we decided
+        to create a project that would summarize text. We figured that this could be a solution to the endless readings given to us.
+        </p> -->
+    </div>
     </div>
   </div>
 </div>
